a4bbd0c00743b1a9d37db3aa551bf04561c3ea81
Krystian Świtała <krystianswitala@gmail.com>
2019-01-07 19:38:14 +0100

    To samo opakowane w klasę.

--- main.php
+++ main.php
@@ -1,78 +1,81 @@
 <?php
 
-const XML_FILE_NAME = '36779.xml';
-const TXT_FILE_NAME = '36779.txt';
-const FTP_URL = 'ftp://user:pass@server.com/';
+class XmlToTxtConverter {
+    const XML_FILE_NAME = '36779.xml';
+    const TXT_FILE_NAME = '36779.txt';
+    const FTP_URL = 'ftp://user:pass@server.com/';
 
-/**
- * Załadowanie XML-a z pliku
- * @param string $fileName nazwa pliku
- * @return SimpleXMLElement
- */
-function loadXMLFile(string $fileName): SimpleXMLElement {
-    if (file_exists($fileName)) {
-        return simplexml_load_file($fileName);
-    } else {
-        return null;
-    }
-}
-
-/**
- * Konwersja z formatu XML do TXT
- * @param SimpleXMLElement $xmlElem
- * @return string
- */
-function xmlToTxtConversion(SimpleXMLElement $xmlElem): string {
-    $txtRes  = '###HIDDEN_TITLE:'.$xmlElem->hidden_title."#\r\n";
-    $txtRes .= '###NAME:'.$xmlElem->name."#\r\n";
-    $txtRes .= '###NACHNAME:'.$xmlElem->nachname."#\r\n";
-    $txtRes .= '###STRASSE:'.$xmlElem->strasse."#\r\n";
-    $txtRes .= '###PLZ:'.$xmlElem->plz."#\r\n";
-    $txtRes .= '###CITY:'.$xmlElem->city."#\r\n";
-    $txtRes .= '###PHONE:'.$xmlElem->phone."#\r\n";
-    $txtRes .= '###EMAIL:'.$xmlElem->email."#\r\n";
-    $txtRes .= '###BEMERKUNGEN:'.$xmlElem->bemerkungen."#\r\n";
-    $txtRes .= '###BEDARF:'.$xmlElem->bedarf."#\r\n";
-    $txtRes .= '###HIDDEN_URL:'.$xmlElem->hidden_url."#\r\n";
-    return $txtRes;
-}
-
-/**
- * Wysłanie danych na serwer FTP
- * @param string $url
- * @param string $content
- * @return bool
- */
-function exportToFtp(string $url, string $content): bool {
-    $fp = fopen($url, "w");
-    if ($fp === false) {
-        return false;
+    /**
+     * Załadowanie XML-a z pliku
+     * @param string $fileName nazwa pliku
+     * @return SimpleXMLElement
+     */
+    private function loadXMLFile(string $fileName): SimpleXMLElement {
+        if (file_exists($fileName)) {
+            return simplexml_load_file($fileName);
+        } else {
+            return null;
+        }
     }
 
-    $writeRes = fwrite($fp, $content);
-    $closeRes = fclose($fp);
-    return ($writeRes === false || $closeRes === false) ? false : true;
-}
+    /**
+     * Konwersja z formatu XML do TXT
+     * @param SimpleXMLElement $xmlElem
+     * @return string
+     */
+    private function xmlToTxtConversion(SimpleXMLElement $xmlElem): string {
+        $txtRes  = '###HIDDEN_TITLE:'.$xmlElem->hidden_title."#\r\n";
+        $txtRes .= '###NAME:'.$xmlElem->name."#\r\n";
+        $txtRes .= '###NACHNAME:'.$xmlElem->nachname."#\r\n";
+        $txtRes .= '###STRASSE:'.$xmlElem->strasse."#\r\n";
+        $txtRes .= '###PLZ:'.$xmlElem->plz."#\r\n";
+        $txtRes .= '###CITY:'.$xmlElem->city."#\r\n";
+        $txtRes .= '###PHONE:'.$xmlElem->phone."#\r\n";
+        $txtRes .= '###EMAIL:'.$xmlElem->email."#\r\n";
+        $txtRes .= '###BEMERKUNGEN:'.$xmlElem->bemerkungen."#\r\n";
+        $txtRes .= '###BEDARF:'.$xmlElem->bedarf."#\r\n";
+        $txtRes .= '###HIDDEN_URL:'.$xmlElem->hidden_url."#\r\n";
+        return $txtRes;
+    }
 
-/**
- * Funkcja główna
- */
-function main() {
-    $xmlElement = loadXMLFile(XML_FILE_NAME);
-//     var_dump($xmlElement);
+    /**
+     * Wysłanie danych na serwer FTP
+     * @param string $url
+     * @param string $content
+     * @return bool
+     */
+    private function exportToFtp(string $url, string $content): bool {
+        $fp = fopen($url, "w");
+        if ($fp === false) {
+            return false;
+        }
 
-    if ($xmlElement !== null) {
-        $txtStr = xmlToTxtConversion($xmlElement);
-        var_dump($txtStr);
+        $writeRes = fwrite($fp, $content);
+        $closeRes = fclose($fp);
+        return ($writeRes === false || $closeRes === false) ? false : true;
+    }
 
-        // Zapis danych...
-        @file_put_contents(TXT_FILE_NAME, $txtStr); // ... do pliku na lokalnym dysku
-        @exportToFtp(FTP_URL.TXT_FILE_NAME, $txtStr); // ... na serwer FTP
+    /**
+     * Funkcja główna
+     */
+    static function main() {
+        $conv = new XmlToTxtConverter();
+        $xmlElement = $conv->loadXMLFile(XmlToTxtConverter::XML_FILE_NAME);
+    //     var_dump($xmlElement);
 
-        // Kasowanie pliku wynikowego
-//         @unlink(TXT_FILE_NAME);
+        if ($xmlElement !== null) {
+            $txtStr = $conv->xmlToTxtConversion($xmlElement);
+            var_dump($txtStr);
+
+            // Zapis danych...
+            @file_put_contents(XmlToTxtConverter::TXT_FILE_NAME, $txtStr); // ... do pliku na lokalnym dysku
+            $conv->exportToFtp(XmlToTxtConverter::FTP_URL . XmlToTxtConverter::TXT_FILE_NAME, $txtStr); // ... na serwer FTP
+
+            // Kasowanie pliku wynikowego
+//             @unlink(XmlToTxtConverter::TXT_FILE_NAME);
+        }
     }
 }
 
 // No to startujemy
-main();
+XmlToTxtConverter::main();
