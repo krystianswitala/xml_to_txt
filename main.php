6e611542ff441a541f9ccaa81ebfe73acd7a7455
Krystian Świtała <k.switala@pozyton.com.pl>
2019-01-07 09:47:54 +0100

    Odpalona propozycja konwersji XML -> TXT.

--- main.php
+++ main.php
@@ -4,22 +4,52 @@ const XML_FILE_NAME = '36779.xml';
 const TXT_FILE_NAME = '36779.txt';
 
 /**
- * Za≥adowanie XML-a z pliku
+ * Za≈Çadowanie XML-a z pliku
  * @param string $fileName nazwa pliku
  * @return SimpleXMLElement
  */
 function loadXMLFile(string $fileName): SimpleXMLElement {
-    $xmlstr = "";
-    
-    return new SimpleXMLElement($xmlstr);
+    if (file_exists($fileName)) {
+        return simplexml_load_file($fileName);
+    } else {
+        return null;
+    }
 }
 
 /**
- * Funkcja g≥Ûwna
+ * Konwersja z formatu XML do TXT
+ * @param SimpleXMLElement $xmlElem
+ * @return string
+ */
+function xmlToTxtConversion(SimpleXMLElement $xmlElem): string {
+    $txtRes  = '###HIDDEN_TITLE:'.$xmlElem->hidden_title."#\r\n";
+    $txtRes .= '###NAME:'.$xmlElem->name."#\r\n";
+    $txtRes .= '###NACHNAME:'.$xmlElem->nachname."#\r\n";
+    $txtRes .= '###STRASSE:'.$xmlElem->strasse."#\r\n";
+    $txtRes .= '###PLZ:'.$xmlElem->plz."#\r\n";
+    $txtRes .= '###CITY:'.$xmlElem->city."#\r\n";
+    $txtRes .= '###PHONE:'.$xmlElem->phone."#\r\n";
+    $txtRes .= '###EMAIL:'.$xmlElem->email."#\r\n";
+    $txtRes .= '###BEMERKUNGEN:'.$xmlElem->bemerkungen."#\r\n";
+    $txtRes .= '###BEDARF:'.$xmlElem->bedarf."#\r\n";
+    $txtRes .= '###HIDDEN_URL:'.$xmlElem->hidden_url."#\r\n";
+    return $txtRes;
+}
+
+/**
+ * Funkcja g≈Ç√≥wna
  */
 function main() {
     $xmlElement = loadXMLFile(XML_FILE_NAME);
-    var_dump($xmlElement);
+//     var_dump($xmlElement);
+
+    if ($xmlElement !== null) {
+        $txtStr = xmlToTxtConversion($xmlElement);
+        var_dump($txtStr);
+
+        // Zapis danych
+        file_put_contents(TXT_FILE_NAME, $txtStr);
+    }
 }
 
 // No to startujemy
